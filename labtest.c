0e5ce601abd379a6a9bc6bfbf6349ffc797fe050
Adam Wolanski <adam.wolanski94@gmail.com>
2015-03-21 23:51:34 +0100

    hotfix1

--- labtest.c
+++ labtest.c
@@ -4,13 +4,13 @@ int main(){
 	int i;
 	
 	for(i=0; i<5; i++)
-		printf("%d",i);
+		printf("%d",i+1);
 
 	return 0;
 }
 
 void print(){
-	printf("print print\n");
+	printf("print\n");
 }
 
 int add(int a, int b){
